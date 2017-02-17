e0a2d7c5342f4be73d01044376cacbb88072fc5d
AgoYaMo <AgoYaMo@Gmail.com>
2017-02-17 11:49:35 +0000

    Agregar archivos de proyecto.

---
+++ Servicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+using System.Diagnostics;
+
+namespace Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceController service = new ServiceController("MSSQL$SQLEXPRESS");
+            //Microsoft SQL Server Management Studio Path
+            ProcessStartInfo exe = new ProcessStartInfo(@"C:\Program Files (x86)\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\Ssms.exe");
+            if (service.Status == ServiceControllerStatus.Stopped)
+            {
+                service.Start();
+            }
+            Process process = Process.Start(exe);
+            process.WaitForExit();
+            service.Stop();
+        }
+    }
+}
